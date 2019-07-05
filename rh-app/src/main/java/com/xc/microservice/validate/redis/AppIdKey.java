40256cb2245833e83c5221f6f4da734ff95dfb30
cunyizhan <vuipxc@163.com>
2019-07-05 18:28:26 +0800

    wxm

--- rh-app/src/main/java/com/xc/microservice/validate/redis/AppIdKey.java
+++
@@ -1,9 +0,0 @@
-package com.xc.microservice.validate.redis;
-
-public class AppIdKey extends BasePrefix {
-
-	public AppIdKey(String prefix) {
-		super(prefix);
-	}
-	public static AppIdKey appid = new AppIdKey("appid:");
-}
--- rh-app/src/main/java/com/xc/microservice/validate/redis/KeyPrefix.java
+++
@@ -1,9 +0,0 @@
-package com.xc.microservice.validate.redis;
-
-public interface KeyPrefix {
-		
-	public int expireSeconds();
-	
-	public String getPrefix();
-	
-}
